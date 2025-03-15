aa3a3b271fd6fb61849a22c4eab8e41b671a607c
Data-analyst-Jay <jaygehlot661@gmail.com>
2025-03-15 14:59:12 +0530

    Built KNN model from scratch and trained on Iris dataset

--- 1 From scratch/1 KNN.py
+++ 1 From scratch/1 KNN.py
@@ -1,6 +1,46 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from collections import Counter
+from sklearn.metrics import accuracy_score
 
 df = pd.read_csv('Required Datasets\\Iris_Datasets.csv')
-print(df.describe())
+
+x = df.iloc[:, :-1].values
+y = df.iloc[:, -1].values
+
+x_train, x_test, y_train, y_test = train_test_split(x,y, test_size=0.3, random_state=0)
+
+sc = StandardScaler()
+x_train = sc.fit_transform(x_train)
+x_test = sc.transform(x_test)
+
+def euclidean_distance(x1, x2):
+    return np.sqrt(np.sum((x1 - x2) ** 2))
+
+class KNN:
+    def __init__(self, k):
+        self.k = k
+
+    def fit(self, x, y):
+        self.x_train = x
+        self.y_train = y
+
+    def predict(self, x):
+        predictions = [self._predict(i) for i in x]
+        return np.array(predictions)
+    
+    def _predict(self, x):
+        distances = [euclidean_distance(x, x_train) for x_train in self.x_train]
+        k_indices = np.argsort(distances)[:self.k]
+        k_nearest_labels = [self.y_train[i] for i in k_indices]
+        most_common = Counter(k_nearest_labels).most_common()
+        return most_common[0][0]
+    
+k = KNN(3)
+k.fit(x_train, y_train)
+predictions = k.predict(x_test)
+print(predictions)
+print(accuracy_score(y_test, predictions))
