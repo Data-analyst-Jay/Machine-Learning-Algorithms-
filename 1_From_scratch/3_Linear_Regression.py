bf5d64b4d10d255a1ea3a5ac66e4298b929b5408
Data-analyst-Jay <jaygehlot661@gmail.com>
2025-03-23 16:07:59 +0530

    Change dataset for linear regression

--- 1_From_scratch/3_Linear_Regression.py
+++ 1_From_scratch/3_Linear_Regression.py
@@ -2,8 +2,9 @@ import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
+from sklearn.preprocessing import StandardScaler
 
-class MeraLR:
+class LR:
     def __init__(self):
         self.m = None
         self.b = None
@@ -24,3 +25,21 @@ class MeraLR:
     def predict(self,X_test):
         return self.m * X_test + self.b
     
+df = pd.read_csv('Required Datasets/diabetes.csv')
+x = df.iloc[:, :-1].values
+y = df.iloc[:, -1].values
+
+x_train, x_test, y_train, y_test = train_test_split(x,y, test_size=0.3, random_state=1)
+
+sc = StandardScaler()
+x_train = sc.fit_transform(x_train)
+x_test = sc.transform(x_test)
+
+model = LR()
+model.fit(x_train,y_train)
+predictions = model.predict(x_test)
+
+print(int(predictions))
+
+acc = accuracy_score(y_test, predictions)
+print(acc)
