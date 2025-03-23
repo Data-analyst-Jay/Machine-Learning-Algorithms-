0aeac02ac79305f264bbb02faa6864216cb3eda3
Data-analyst-Jay <jaygehlot661@gmail.com>
2025-03-23 18:04:31 +0530

    Implemented Naive Bayes from scratch and implemented it with output of 79% accuracy

--- 1_From_scratch/2_Naive_Bayes.py
+++ 1_From_scratch/2_Naive_Bayes.py
@@ -3,6 +3,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
+from sklearn.preprocessing import StandardScaler
 
 class Naive_Bayes:
     def fit(self, X, y):
@@ -10,4 +11,54 @@ class Naive_Bayes:
         self._classes = np.unique(y)
         n_classes = len(self._classes)
 
+        self._mean = np.zeros((n_classes, n_features), dtype=np.float64)
+        self._var = np.zeros((n_classes, n_features), dtype=np.float64)
+        self._priors = np.zeros(n_classes, dtype=np.float64)
+
+        for idx, c in enumerate(self._classes):
+            X_c = X[y == c]
+            self._mean[idx, :] = X_c.mean(axis=0)
+            self._var[idx, :] = X_c.var(axis=0)
+            self._priors[idx] = X_c.shape[0] / float(n_samples)
+
+    def predict(self, X):
+        y_pred = [self._predict(x) for x in X]
+        return np.array(y_pred)
+
+    def _predict(self, x):
+        posteriors = []
+
+        for idx, c in enumerate(self._classes):
+            prior = np.log(self._priors[idx])
+            posterior = np.sum(np.log(self._pdf(idx, x)))
+            posterior = posterior + prior
+            posteriors.append(posterior)
+
+        return self._classes[np.argmax(posteriors)]
+
+    def _pdf(self, class_idx, x):
+        mean = self._mean[class_idx]
+        var = self._var[class_idx]
+        numerator = np.exp(-((x - mean) ** 2) / (2 * var))
+        denominator = np.sqrt(2 * np.pi * var)
+        return numerator / denominator
     
+    def accuracy(self,y_true, y_pred):
+        accuracy = np.sum(y_true == y_pred) / len(y_true)
+        return accuracy
+
+df = pd.read_csv("Required Datasets/diabetes.csv")
+
+X = df.iloc[:,:-1].values
+y = df.iloc[:,-1].values
+
+sc = StandardScaler()
+# x = sc.fit_transform(X)
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
+
+nb = Naive_Bayes()
+nb.fit(X_train, y_train)
+predictions = nb.predict(X_test)
+
+print("Naive Bayes classification accuracy", nb.accuracy(y_test, predictions))
